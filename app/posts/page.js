9de1c2daaa3e682528290caf9a6125e76cc22330
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-05-09 14:23:50 -0500

    added metadata stuff in the lower page.js

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 
+// this is metadata for SEO etc.  this would be static metadata
+export const metadata = {
+  title: "Home",
+};
+
+// DYNAMIC metadata
+export async function generateMetadata({ params, searchParams }) {
+  const product = await getProduct(params.id);
+  return { title: product.title };
+}
+
 export default class page extends Component {
   render() {
     return <div>Post</div>;
